512978620ea3db4e309678a0f30fa70d132224cb
yeshengqiang <673089899@qq.com>
2016-10-28 14:12:09 +0800

    配置文件

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+	browserSync = require('browser-sync').create(),
+	reload = browserSync.reload();
+
+gulp.task('sync',function(){
+	browserSync.init({
+		server:{
+			baseDir:'./',
+			index: "index.html"
+		},
+		port:9000,
+		browser:'chrome'
+	});
+	gulp.watch('*').on('change',reload);
+});
+
+gulp.task('default',['sync']);

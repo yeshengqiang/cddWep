a3f8effb1271aab753cd1903690967be3510bb39
Hyhello <673089899@qq.com>
2016-11-02 14:06:21 +0800

    配置信息

--- views/config.js
+++ views/config.js
@@ -6,7 +6,7 @@
 define(function(require){
 	var app = require('../app');
 	//公共入口
-	app.constant('url', 'http://121.43.101.74:8080/warehouse');
+	app.constant('url', 'http://112.124.101.153:8089/warehouse');
 	//app.constant('url', 'http://:80/warehouse');
 	//app.constant('url', 'http://192.168.10.189:8080/CddWep');
 });

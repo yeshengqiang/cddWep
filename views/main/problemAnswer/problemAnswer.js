62b269840172df197593f8aac51d27f74346204e
Hyhello <673089899@qq.com>
2016-11-03 11:17:23 +0800

    修改类型

--- views/main/problemAnswer/problemAnswer.js
+++ views/main/problemAnswer/problemAnswer.js
@@ -1,47 +1,41 @@
 /**
  *  作者：makaiqin
- *	时间：2016-08-10
- *	描述：问题反馈
+ *  时间：2016-08-10
+ *  描述：问题反馈
  */
-define(function(require){
+define(function(require) {
     var app = require('../../../app');
     //过滤器
-    app.filter('typeFormat', function () {
-        return function (inp) {
+    app.filter('typeFormat', function() {
+        return function(inp) {
             var info = "";
             switch (inp) {
                 case '1':
-                    info = '类型1';
+                    info = '系统问题';
                     break;
                 case '2':
-                    info = '类型2';
+                    info = '系统优化';
                     break;
                 case '3':
-                    info = '类型3';
+                    info = '操作流程';
                     break;
             }
             return info;
         };
     });
-
-    app.controller('problemAnswerCrl',['$scope','url','$http','$location',function($scope,url,$http,$location){
-
+    app.controller('problemAnswerCrl', ['$scope', 'url', '$http', '$location', function($scope, url, $http, $location) {
         //初始化
         $scope.searchData = {};
         //获取分页数据
-        var answerList = function (page, callback) {
+        var answerList = function(page, callback) {
             var param = app.get('checkValue').dateRangeFormat($scope.searchData);
-            $http.post(url + '/suggestion/showPageList', $.extend({},page, param)).success(callback)
+            $http.post(url + '/suggestion/showPageList', $.extend({}, page, param)).success(callback)
         };
         $scope.searchPaginator = app.get('Paginator').list(answerList, 6);
-
         //问题反馈
-        $scope.feedback = function(){
-
+        $scope.feedback = function() {
             $location.path('/main/problemAnswer/problemWrite');
         };
-
-        yMake.fn.autoHeight('.bgWhite',45);
-
+        yMake.fn.autoHeight('.bgWhite', 45);
     }]);
 });

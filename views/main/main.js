bc512544d44b01c3aea122259f317a0eb2bdbb67
Hyhello <673089899@qq.com>
2016-11-10 09:40:56 +0800

    分号

--- views/main/main.js
+++ views/main/main.js
@@ -3,15 +3,13 @@
  *	时间：2016-08-08
  *	描述：路由首页
  */
-define(function(require){
-	var app = require('../../app');
-
-	app.controller('mainCrl',['$scope','$rootScope',function($scope,$rootScope){
-        $(function(){
-            setTimeout(function () {
-                yMake.fn.autoHeight('.leftBox',16);
-            },10)
+define(function(require) {
+    var app = require('../../app');
+    app.controller('mainCrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+        $(function() {
+            setTimeout(function() {
+                yMake.fn.autoHeight('.leftBox', 16);
+            }, 10);
         });
-
-	}]);
+    }]);
 });
